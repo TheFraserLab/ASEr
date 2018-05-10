7827e53857deee3382f1c926a477f148cbd383c6
petercombs <pcombs@stanford.edu>
2018-05-10 16:15:15 -0700

    Merged

--- bin/GetGeneASEbyReads.py
+++ bin/GetGeneASEbyReads.py
@@ -3,13 +3,17 @@ from pysam import AlignmentFile
 from argparse import ArgumentParser, FileType
 from collections import defaultdict, Counter
 from multiprocessing import Pool, cpu_count
+from scipy.stats import binom_test
 from sys import stdout
 from os import path
+from itertools import chain
 import ASEr.logme as lm
-from math import log2, sqrt
+from math import log10, log2, sqrt
 import pickle
 import subprocess
 import re
+
+ASSIGNED_READS = {}
 
 try:
     from progressbar import ProgressBar as pbar
@@ -65,7 +69,7 @@ def get_snps(snpfile):
     return snps
 
 def get_gene_coords(gff_file, id_name, feature_type='exon'):
-    if path.exists(gff_file + '.pkl'):
+    if False and path.exists(gff_file + '.pkl'):
         return pickle.load(open(gff_file + '.pkl', 'rb'))
     gene_coords = defaultdict(lambda : [None, set()])
     for line in open(gff_file):
@@ -100,7 +104,7 @@ def get_gene_coords(gff_file, id_name, feature_type='exon'):
     gene_coords_out = {}
     for entry in gene_coords:
         gene_coords_out[entry] = gene_coords[entry]
-    pickle.dump(gene_coords_out, open(gff_file + '.pkl', 'wb'))
+    #pickle.dump(gene_coords, open(gff_file + '.pkl', 'wb'))
     return gene_coords
 
 def get_ase_by_coords(chrom, coords, samfile, snp_dict):
@@ -136,7 +140,7 @@ def get_ase_by_coords(chrom, coords, samfile, snp_dict):
             # UTR
             read_results['missed exon boundaries'] += 1
             continue
-        phase = get_phase(read, snps_on_chrom)
+        phase = ASSIGNED_READS.setdefault(read.qname, get_phase(read, snps_on_chrom) )
         phases[read.qname].add(phase)
         # Note that pairs of the same read should have the same qname.
 
@@ -164,6 +168,9 @@ def pref_index(ref, alt):
 
 def log2ase(ref, alt):
     return log2(alt/ref)
+
+def log2ase_offset(ref, alt):
+    return log2((alt + 5)/(ref+5))
 
 def ratio(ref, alt):
     return alt/ref
@@ -200,11 +207,35 @@ def wilson95_pref(ref, alt):
 
     return 2 * p - 1
 
+def binom_pval_polarized(ref, alt):
+    """A polarized version of the log10 pvalue
+
+    Will be negative if more reference than alternative, positive if more
+    alternative than reference"""
+
+    pval = binom_test([ref, alt], p=0.5)
+    if ref > alt:
+        return log10(pval) if pval>0 else -30
+    else:
+        return -log10(pval) if pval>0 else 30
+
+def beta_binom(ref, alt, all_ref, all_alt):
+    x = alt
+    n = ref + alt
+    a = all_ref
+    b = all_alt
+    lnanswer = gammaln(n+1) + gammaln(x+a) + gammaln(n-x+b) + gammaln(a+b) - \
+            (gammaln(x+1) + gammaln(n-x+1) + gammaln(a) + gammaln(b) + gammaln(n+a+b))
+    return lnanswer
+
+
 ase_fcns = {
     'wilson95': wilson95_pref,
     'pref_index': pref_index,
     'log2': log2ase,
     'ratio': ratio,
+    'log10pval': binom_pval_polarized,
+    'log2offset': log2ase_offset,
 }
 
 def get_lib_size(reads):
@@ -212,12 +243,16 @@ def get_lib_size(reads):
     libSize = str(subprocess.check_output(arg,shell=True))
     libSize=re.sub(r'b\'([0-9]+)(\\.*)',r'\1',libSize)
     return libSize
-			
+
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('snp_file')
     parser.add_argument('gff_file')
     parser.add_argument('reads')
+    parser.add_argument('--assign-all-reads', default=False,
+            action='store_true',
+            help='Get an overall accounting of which reads map to reference, '
+            'alternative, or other')
     parser.add_argument('--max-jobs', '-p', default=0, type=int,
             help=''
             )
@@ -263,6 +298,45 @@ if __name__ == "__main__":
             if 'finish' in dir(prog):
                 prog.finish()
     else:
+        if args.assign_all_reads:
+            phase_counters = Counter()
+            references = list(reads.references)
+            unpaired_reads = [{}, {}] #Unpaired Read 1s, Unpaired Read 2s
+            for read in pbar(max_value=reads.mapped)(reads):
+                qname = read.qname
+                phase = get_phase(
+                        read,
+                        snp_dict[references[read.reference_id]]
+                        )
+                if qname in unpaired_reads[read.is_read1]:
+                    other_phase = unpaired_reads[read.is_read1].pop(qname)
+                    phase_set = set([phase, other_phase])
+                    phase_set.discard(None)
+                    # Removes None if any, so a paired end read with no information doesn't
+                    # invalidate the end that does have a clear phase.
+                    if len(phase_set) == 1:
+                        # Unambiguously phased
+                        phase_counters[phase_set.pop()] += 1
+                    elif len(phase_set) == 0:
+                        phase_counters[None] += 1
+                    else:
+                        phase_counters[0] += 1
+                else:
+                    #unpaired_reads[True] = Read 2
+                    #unpaired_reads[False] = Read 1
+                    unpaired_reads[read.is_read2][qname] = phase
+
+
+            print(len(unpaired_reads[0]), " unpaired read 1s")
+            print(len(unpaired_reads[1]), " unpaired read 2s")
+
+            for phase in chain(unpaired_reads[0].values(),
+                    unpaired_reads[1].values()):
+                phase_counters[phase] += 1
+            print("# All read phases: ", phase_counters, file=args.outfile,
+                    end='\n')
+            args.outfile.flush()
+
         prog = pbar()
         for gene in prog(gene_coords):
             ase_vals[gene] = get_ase_by_coords(
@@ -281,7 +355,7 @@ if __name__ == "__main__":
     for gene in sorted(ase_vals):
         avg = ase_vals[gene]
         # Not "average", "ase vals for gene"
-        if (min(avg[1], avg[-1]) > args.min_reads_per_allele) and (avg[1] + avg[-1] > args.min_reads_per_gene):
+        if (min(avg[1], avg[-1]) >= args.min_reads_per_allele) and (avg[1] + avg[-1] >= args.min_reads_per_gene):
             ase_fcn = ase_fcns[args.ase_function]
             ase_val = ase_fcn(avg[-1], avg[1])
         else:
